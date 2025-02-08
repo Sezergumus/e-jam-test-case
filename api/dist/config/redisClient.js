e4893e4c02f909747b9044dd7c4191fec38762d8
Sezer Gümüş <sezer2004@gmail.com>
2025-02-09 02:04:15 +0300

    fixed api

--- api/dist/config/redisClient.js
+++ api/dist/config/redisClient.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const ioredis_1 = __importDefault(require("ioredis"));
-const client = new ioredis_1.default();
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const client = new ioredis_1.default(process.env.REDIS_URL);
 client.on("connect", () => {
     console.log("Connected to Redis");
 });
 client.on("error", (err) => {
-    console.log("Redis error:", err);
+    console.error("Redis error:", err);
 });
 exports.default = client;
